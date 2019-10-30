519f00c77e52157d72e9d4e27f8a6c133ba4e022
rowanq <rowanqueue@gmail.com>
2019-10-30 13:45:20 -0400

    actual adjacency now

--- Assets/Scripts/Plants/Plant.cs
+++ Assets/Scripts/Plants/Plant.cs
@@ -15,20 +15,32 @@ public class Plant : MonoBehaviour
     public List<LineRenderer> lines = new List<LineRenderer>();//shows the plants this plant supports
     public List<Plant> supportedBy = new List<Plant>();
     public List<Plant> dependents = new List<Plant>();
+    public float[] needs = new float[] { 0, 0, 0, 0, 0, 0 };
+    public bool met = false;
+    MeshRenderer mr;
+    public Material good;
+    public Material bad;
 
     GameObject child;
+    float size = 1;
     int age;
     float growthStage;
 
     // Start is called before the first frame update
     void Awake()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        met = CheckNeeds();
+        if (mr)
+        {
+            mr.material = met ? good : bad;
+            child.transform.localScale = met ? new Vector3(1, 1, 1)*size : new Vector3(0.5f, 0.5f, 0.5f)*size;
+        }
     }
     public void SetType(PlantType newType)
     {
@@ -37,6 +49,29 @@ public class Plant : MonoBehaviour
         {
             string modelName = prefabs[(int)type];
             child = Instantiate(Resources.Load(modelName), transform) as GameObject;
+            size = child.transform.localScale.x;
+            mr = child.GetComponent<MeshRenderer>();
+            switch (type)
+            {
+                case PlantType.Grass:
+                    needs = new float[6];
+                    needs[1] = 2;
+                    break;
+                case PlantType.Shrub:
+                    needs = new float[6];
+                    needs[1] = 4;
+                    needs[2] = 2;
+                    break;
+                case PlantType.Tree:
+                    needs = new float[6];
+                    needs[1] = 8;
+                    needs[2] = 4;
+                    needs[3] = 2;
+                    break;
+                default:
+                    needs = new float[6];
+                    break;
+            }
         }
         UpdateDependecies();
     }
@@ -118,5 +153,29 @@ public class Plant : MonoBehaviour
                 }
                 break;
         }
+    }
+    public bool CheckNeeds()
+    {
+        bool met = false;
+        float[] needsMet = new float[6];
+        foreach(Plant plant in supportedBy)
+        {
+            int index = (int)plant.type;
+            needsMet[index] += 1;
+        }
+        for(int i =0; i < needs.Length; i++)
+        {
+            if (needsMet[i] >= needs[i])
+            {
+                met = true;
+                continue;
+            }
+            else
+            {
+                met = false;
+                break;
+            }
+        }
+        return met;
     }
 }
